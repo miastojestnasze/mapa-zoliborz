ad1890db3ec08daf21b10a54a8aa0f46644baf61
drzamich <drzamich@gmail.com>
2021-04-26 11:51:05 +0000

    Deploying to gh-pages from  @ ddb2855d8f946307223556096869d4fb6acdc769 🚀

--- _next/static/--bCSSPaIA7GyKWoBlsGK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-018eadb310efa09b0b3f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9afba3ea1eff41c17e84.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
